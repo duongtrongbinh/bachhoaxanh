5935fd1b69fe02ccc470424f1a6a0a4b21ff929c
duongtrongbinh <137783978+duongtrongbinh@users.noreply.github.com>
2024-06-06 18:33:01 +0700

    bind repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Http\Repositories\Repository;
+use App\Http\Repositories\RepositoryInteface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(RepositoryInteface::class, Repository::class);
     }
 
     /**
